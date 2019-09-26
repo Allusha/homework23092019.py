f36eaa52bb53222650c4fcafc8d66491f53e5a25
Allusha <siempreallal@gmail.com>
2019-09-26 16:36:37 +0300

    first task

---
+++ homework26092019.py
@@ -0,0 +1,11 @@
+test = 'test1, test2, test3, test4, test5'
+index = int(input('Type a number:'))
+test2 = test.split(', ')
+del test2[index-1]
+test3 = ', '.join(test2)
+print(test3)
+
+str = 'test1, test2, test3, test4, test5'
+index = int(input('Type a number:'))
+todel = f'test{index}, '
+print(str.replace(todel,''))
