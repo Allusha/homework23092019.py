22e8809be0b26cf1862b4d9652dc0cae74ee99de
Allusha <siempreallal@gmail.com>
2019-09-26 16:59:37 +0300

    second task

--- homework26092019.py
+++ homework26092019.py
@@ -9,3 +9,8 @@ str = 'test1, test2, test3, test4, test5'
 index = int(input('Type a number:'))
 todel = f'test{index}, '
 print(str.replace(todel,''))
+
+# 2 task
+url = input('Put the URL:')
+dom = url.split('/')
+print(dom[2])
